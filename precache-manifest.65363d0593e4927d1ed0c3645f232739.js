37839dd0b58f6be29521c807437034a72b668818
Songpon Yonpan <s.yonpan@gmail.com>
2022-07-20 19:09:27 +0700

    Updates

---
+++ precache-manifest.65363d0593e4927d1ed0c3645f232739.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cfe97ccd083825220d1",
+    "url": "/static/css/main.ba0b7754.chunk.css"
+  },
+  {
+    "revision": "1cfe97ccd083825220d1",
+    "url": "/static/js/main.1cfe97cc.chunk.js"
+  },
+  {
+    "revision": "e4c123256ec82141fcb1",
+    "url": "/static/js/1.e4c12325.chunk.js"
+  },
+  {
+    "revision": "2871f488c813cba3fd0c",
+    "url": "/static/css/2.de87d744.chunk.css"
+  },
+  {
+    "revision": "2871f488c813cba3fd0c",
+    "url": "/static/js/2.2871f488.chunk.js"
+  },
+  {
+    "revision": "95cde843341cbe58b5ad",
+    "url": "/static/js/runtime~main.95cde843.js"
+  },
+  {
+    "revision": "7c9de6024201acf78e04532c58b70e59",
+    "url": "/static/media/recyclelogo.7c9de602.png"
+  },
+  {
+    "revision": "150f24ceb8e55d90c4d26bb5e50f6bb3",
+    "url": "/index.html"
+  }
+];
